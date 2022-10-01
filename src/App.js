23f8960265f93d132025f12bc9dec3c24c9766b2
idna001 <arv001@web.de>
2022-10-01 12:15:13 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>A&A Match</h1>
+      <button>Start the Game</button>
+    </div>
+  );
+}
+
+export default App
