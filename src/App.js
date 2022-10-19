f659b06d8c990a59e7711455e0e4462e73739e7b
idna001 <arv001@web.de>
2022-10-19 15:32:55 +0200

    Add time in LocalStorage

--- src/App.js
+++ src/App.js
@@ -14,15 +14,20 @@ const cardImages = [
 function App() {
     const [cards, setCards] = useState([])
     const [turns, setTurns] =useState(0)
+
     const [choiceOne, setChoiceOne] = useState(null)
     const [choiceTwo, setChoiceTwo] = useState(null)
     const [disabled, setDisabled] = useState(false)
+    const [matched, setMatched] = useState(0)
+
+
     const [highScore, setHighScore] = useState(0)
     const [fastest_run, setFastestRun] = useState(0)
-    const [matched, setMatched] = useState(0)
-    const [celebrationStatus, setCelebrationStatus] = useState(false)
+
     const [elapsedTime, setTime] = useState(undefined)
     const [intervalId, setIntervalId] = useState(undefined)
+
+    const [celebrationStatus, setCelebrationStatus] = useState(false)
 
     //shuffle
     const shuffledCards = () => {
@@ -97,13 +102,13 @@ function App() {
     useEffect(()=>{
         if (matched === cards.length && turns){
             // Game over
-            const m_highscore = window.localStorage.getItem("highscore")
-            const fastest_run = window.localStorage.getItem("elapsedTime")
+            const m_highscore = window.localStorage.getItem("highScore")
+            const m_fastest_run = window.localStorage.getItem("fastest_run")
 
             handleTime(false)
-            if (m_highscore === null || turns < Number(m_highscore) && elapsedTime < Number(fastest_run) ){
-                // New highscore
-                window.localStorage.setItem("highscore", turns)
+            if (m_highscore === null || turns < Number(m_highscore) && elapsedTime < Number(m_fastest_run) ){
+                // New highScore
+                window.localStorage.setItem("highScore", turns)
                 window.localStorage.setItem("fastest_run", elapsedTime)
                 soundEffect.src = "audio/celebration.mp3"
                 soundEffect.play()
@@ -117,17 +122,17 @@ function App() {
 
     useEffect(() => {
         shuffledCards()
-        // Load highscore value from localstorage
-        const m_highscore = window.localStorage.getItem("highscore") || 0
+        // Load highScore value from localstorage
+        const m_highscore = window.localStorage.getItem("highScore") || 0
 		setHighScore(m_highscore)
 
-        const  fastest_run = window.localStorage.getItem("elapsedTime") || 0
-        setTime(fastest_run)
+        const m_fastest_run = window.localStorage.getItem("fastest_run") || 0
+        setFastestRun(m_fastest_run)
 }, [])
 
 return (
     <div className="App">
-        {celebrationStatus && <Celebration highscore={highScore} time={elapsedTime}/>}
+        {celebrationStatus && <Celebration Highscore={highScore} elapsedTime={elapsedTime}/>}
         <h1>A&A Match</h1>
         <button onClick={shuffledCards}>New Game</button>
 
@@ -142,7 +147,7 @@ return (
                     ))}
                 </div>
         <p>Turns: {turns}</p>
-        <p>HighScore: {highScore} by {elapsedTime}</p>
+        <p>HighScore: {highScore} by {fastest_run}</p>
         <p>Time Elapsed: {elapsedTime || "Not started"}</p>
 
         <div id="views">
