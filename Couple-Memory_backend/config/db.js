d3a131fae10f082876d485afde7702eb7036eb8e
Saurajit <saura8668@gmail.com>
2023-10-07 21:20:15 +0530

    07-10-2023

--- Couple-Memory_backend/config/db.js
+++ Couple-Memory_backend/config/db.js
@@ -1,9 +1,16 @@
+require('dotenv').config(); // Load environment variables from .env file
 const mongoose = require('mongoose');
-const mongoURI="mongodb://localhost:27017/viewcount"
+const mongoURI = `mongodb+srv://saura8668:${process.env.USER_KEY}@cluster0.c48azqk.mongodb.net/viewcount`;
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected ${conn.connection.host}-${conn.connection.port}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+  }
+};
 
-const connectDB = ( async() => {
-        const conn =  await mongoose.connect(mongoURI)
-        console.log(`MongoDB Connected ${conn.connection.host}-${conn.connection.port}`)
-})
-
-module.exports= connectDB
+module.exports = connectDB;
--- Couple-Memory_backend/index.js
+++ Couple-Memory_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+
+
 const connectDB = require('./config/db')
 connectDB();
 
