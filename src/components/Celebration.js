cda110e715f5b938f0fa979b20ea483029b8f9ba
Bhumika Bachchan <bhumikabachchan11315@gmail.com>
2023-10-09 22:22:06 +0530

    tried adding web-share api

--- src/components/Celebration.js
+++ src/components/Celebration.js
@@ -1,34 +1,76 @@
 import "./Celebration.css";
-import React, { useRef } from 'react';
-import html2canvas from 'html2canvas';
+import React, { useRef, useState } from "react";
+import html2canvas from "html2canvas";
 
-export default function Celebration({highscore, time}){
-    const highScoreRef = useRef(null);
+export default function Celebration({ highscore, time }) {
+  const highScoreRef = useRef(null);
+  const [buttonText, setButtonText] = useState("Take Screenshot");
+  const [screenshotImage, setScreenshotImage] = useState(null);
 
-  const takeScreenshot = () => {
-    if (!highScoreRef.current) {
-      console.error("Element not found.");
-      return;
+  const handleScreenshot = () => {
+    if (buttonText === "Take Screenshot") {
+      if (!highScoreRef.current) {
+        console.error("Element not found.");
+        return;
+      }
+
+      html2canvas(highScoreRef.current, {
+        useCORS: true,
+        backgroundColor: "black",
+      })
+        .then((canvas) => {
+          const image = canvas.toDataURL("image/jpeg");
+          setScreenshotImage(image); 
+          setButtonText("Share Screenshot");
+          const a = document.createElement("a");
+          a.href = image;
+          a.download = "high_score_screenshot.jpeg";
+          a.click();
+        })
+        .catch((error) => {
+          console.error("Error capturing screenshot:", error);
+        });
+    } else {
+      handleShare();
     }
-
-    html2canvas(highScoreRef.current, { useCORS: true , backgroundColor:"black"}).then((canvas) => {
-      const image = canvas.toDataURL("image/jpeg");
-      const a = document.createElement("a");
-      a.href = image;
-      a.download = "high_score_screenshot.jpeg";
-      a.click();
-    }).catch((error) => {
-      console.error("Error capturing screenshot:", error);
-    });
   };
-   return (
-    <div className="celebration-container" >
-        <div className="celebration-notification" >
-            <p ref={highScoreRef}> Highscore achieved: <span>{highscore}</span><br/>Time Taken: <span>{time}s</span></p>
-            <div className="button">
-                <button onClick={takeScreenshot}>Take Screenshot</button>
-            </div>
+
+  const handleShare = () => {
+    if (navigator.share && screenshotImage) {
+      navigator
+        .share({
+          title: "High Score Screenshot",
+          text: `I achieved a high score of ${highscore} in A&A Match!`,
+          url: screenshotImage,
+        })
+        .then(() => {
+          console.log("Share successful");
+        })
+        .catch((error) => {
+          console.error("Error sharing:", error);
+        });
+    } else {
+      if (!navigator.share) {
+        alert("Sharing is not supported on this device/browser.");
+      } else {
+        alert("the screenshot image is not available");
+      }
+    }
+  };
+
+  return (
+    <div className="celebration-container">
+      <div className="celebration-notification">
+        {/* <p ref={highScoreRef}> Highscore achieved: <span>{highscore}</span><br/>Time Taken: </p> */}
+        <p ref={highScoreRef}>
+          <h3>A&A Match</h3>Highscore achieved: <span>{highscore}</span>
+          <br />
+        </p>
+        {/* <p><span>{time}s</span></p> */}
+        <div className="button">
+          <button onClick={handleScreenshot}>{buttonText}</button>
         </div>
+      </div>
     </div>
-   ) 
+  );
 }
