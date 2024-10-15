114d6bd88921f60ccb0705a9bca89b2bf0582375
Andreas V <46223359+idna001@users.noreply.github.com>
2024-10-15 22:58:19 +0200

    Fix consitency from sonarcloud try error

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -15,12 +15,21 @@ export default function SingleCard({card, handleChoice, flipped, disabled}) {
                     <img className="front-background" src={card.src} alt="card front blur background" width="200" height="200"   />
                     <img className="front" src={card.src} alt="card front" width="200" height="200"   />
                 </div>
-                <img
-                    className="back"
-                    src="/img/cover.png"
-                    onClick={handleClick}
-                    alt="card back"
-                    width="200" height="198"                 />
+                <img className="back"
+                     src="/img/cover.png"
+                     onClick={handleClick}
+                     onKeyDown={(e) => {
+                        if (e.key === "Enter" || e.key === " ") {
+                            handleClick();
+                        }
+                     }}
+                     alt="card back"
+                     width="200"
+                     height="198"
+                     tabIndex="0" // Allows the image to be focused with the keyboard
+                     role="button" // Declares it as a button for screen readers
+                />
+
             </div>
         </div>
     )
