21e4da1299af4c52fe94834b9ac4ba67d0b90965
Andreas V <46223359+idna001@users.noreply.github.com>
2024-10-16 19:40:23 +0200

    Fix consitency from sonarcloud try error

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,36 +1,51 @@
-import './SingleCard.css'
+import './SingleCard.css';
+import PropTypes from 'prop-types';
 
-export default function SingleCard({card, handleChoice, flipped, disabled}) {
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
 
     const handleClick = () => {
         if (!disabled) {
-            handleChoice(card)
+            handleChoice(card);
         }
-    }
+    };
 
     return (
         <div className={`card ${flipped && card.matched ? "animation" : ""}`}>
             <div className={flipped ? "flipped" : ""}>
-                <div id='front' className='front' style={{display: "flex"}}>
-                    <img className="front-background" src={card.src} alt="card front blur background" width="200" height="200"   />
-                    <img className="front" src={card.src} alt="card front" width="200" height="200"   />
+                <div id='front' className='front' style={{ display: "flex" }}>
+                    <img className="front-background" src={card.src} alt="card front blur background" width="200" height="200" />
+                    <img className="front" src={card.src} alt="card front" width="200" height="200" />
                 </div>
-                <img className="back"
-                     src="/img/cover.png"
-                     onClick={handleClick}
-                     onKeyDown={(e) => {
+                <button
+                    className="back"
+                    onClick={handleClick}
+                    onKeyDown={(e) => {
                         if (e.key === "Enter" || e.key === " ") {
                             handleClick();
                         }
-                     }}
-                     alt="card back"
-                     width="200"
-                     height="198"
-                     tabIndex="0" // Allows the image to be focused with the keyboard
-                     role="button" // Declares it as a button for screen readers
-                />
-
+                    }}
+                    aria-label="card back"
+                    style={{ background: 'none', border: 'none', padding: 0 }}
+                >
+                    <img
+                        src="/img/cover.png"
+                        alt="card back"
+                        width="200"
+                        height="198"
+                    />
+                </button>
             </div>
         </div>
-    )
+    );
 }
+
+// PropTypes Validation
+SingleCard.propTypes = {
+    card: PropTypes.shape({
+        src: PropTypes.string.isRequired,
+        matched: PropTypes.bool.isRequired,
+    }).isRequired,
+    handleChoice: PropTypes.func.isRequired,
+    flipped: PropTypes.bool.isRequired,
+    disabled: PropTypes.bool.isRequired,
+};
